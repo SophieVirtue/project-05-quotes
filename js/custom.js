7b3b2e24a97991db683c55a1c84bd33532069757
Sophie Virtue <sophie.virtue@gmail.com>
2018-11-21 19:10:34 -0800

    Add responsive style to about page

--- js/custom.js
+++ js/custom.js
@@ -9,14 +9,26 @@
     function getQuote(){
         $.ajax({
             method: 'GET',
-            url: qod_vars.rest_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1'
+            url: qod_vars.rest_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1', 
         }).done(function(data){
-            //append content to the DOM e.g. replace the quote content with rest api content TODO refer to instanews (no loops or filtering needed - just replacing data)
-            console.log(data);
 
-        }).fail(function(err){
-            //Append a message for the user or alert a message saying something went wrong TODO
-            console.log(err);
+            $('.post').empty();
+
+            $('.post').append(
+            `<div class="entry-content">
+             ${data[0].content.rendered}
+            </div>`
+            `<div class="entry-meta">
+                <h2 class="entry-title">&mdash; ${data[0].title.rendered}</h2>
+                <span class="source">
+                    ,<a href="${data[0]._qod_quote_source_url}"> ${data[0]._qod_quote_source}</a>
+                </span>
+            </div>`);
+            
+            //append content to the DOM e.g. replace the quote content with rest api content TODO refer to instanews (no loops or filtering needed - just replacing data)
+            }).fail(function(err){
+            $(`.post`).empty();
+            $(`.post`).append('<p>Apologies, this page is not loading...</p>');
         });
     }
     });
