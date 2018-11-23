c3955c675edee524ddac9d700072ad6ba667cf27
Sophie Virtue <sophie.virtue@gmail.com>
2018-11-23 11:52:00 -0800

    Add style to submit form

--- page-submit.php
+++ page-submit.php
@@ -19,23 +19,23 @@ get_header(); ?>
                     <div class="quote-submission-wrapper">
                     
                         <form name="quoteForm" id="quote-submission-form">
-                            <div>
+                            <div class="label-input">
                                 <label for="quote-author">Author of Quote</label>
                                 <input type="text" name="quote_author" id="quote-author">
                             </div>
-                            <div>
+                            <div class="label-input">
                                 <label for="quote-content">Quote</label>
                                 <textarea name="quote_content" id="quote-content" rows="3" columns="20"></textarea>
                             </div>
-                            <div>
+                            <div class="label-input">
                                 <label for="quote-source">Where did you find this quote? (e.g. book name)</label>
                                 <input type="text" name="quote_source" id="quote-source">
                             </div>
-                            <div>
+                            <div class="label-input">
                                 <label for="quote-source-url">Provide the URL of the quote source, if available.</label>
                                 <input type="url" name="quote_source_url" id="quote-source-url">
                             </div>
-                            <input type="submit" value="Submit Quote">
+                            <input class="submit-button" type="submit" value="Submit Quote">
                         </form>
                     </div>
 
